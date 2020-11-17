04a747ba1d58904d09b65454f6c931b540581962
liangguiquan <969511847@qq.com>
2020-11-17 13:49:41 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/ant-design-vue.js'   // vue add ant design  使用这个命令行引入 ant-design-vue
+import '@/assets/css/reset.css'
 
 Vue.config.productionTip = false
 
